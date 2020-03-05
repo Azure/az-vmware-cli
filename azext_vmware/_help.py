9bacb08ceee499862e4d037dcbd2cbec2054d09d
Cameron Taggart <cameron.taggart@gmail.com>
2020-03-05 13:53:17 -0600

    complete help documentation (#14)

--- azext_vmware/_help.py
+++ azext_vmware/_help.py
@@ -13,27 +13,87 @@ helps['vmware'] = """
     short-summary: Commands to manage private clouds.
 """
 
-helps['vmware create'] = """
+helps['vmware private-cloud'] = """
+    type: group
+    short-summary: Commands to manage private clouds.
+"""
+
+helps['vmware cluster'] = """
+    type: group
+    short-summary: Commands to manage clusters in a private cloud.
+"""
+
+helps['vmware cluster create'] = """
+    type: command
+    short-summary: Create a cluster in a private cloud.
+"""
+
+helps['vmware cluster delete'] = """
+    type: command
+    short-summary: Delete a cluster in a private cloud.
+"""
+
+helps['vmware cluster list'] = """
+    type: command
+    short-summary: List clusters in a private cloud.
+"""
+
+helps['vmware cluster show'] = """
+    type: command
+    short-summary: Show details of a cluster in a private cloud.
+"""
+
+helps['vmware cluster update'] = """
+    type: command
+    short-summary: Update a cluster in a private cloud.
+"""
+
+helps['vmware private-cloud addauthorization'] = """
+    type: command
+    short-summary: Add an authorization to the ExpressRoute for a private cloud.
+"""
+
+helps['vmware private-cloud addidentitysource'] = """
+    type: command
+    short-summary: Add a vCenter Single Sign On Identity Source to a private cloud.
+"""
+
+helps['vmware private-cloud create'] = """
     type: command
     short-summary: Create a private cloud.
 """
 
-helps['vmware list'] = """
-    type: command
-    short-summary: List private clouds.
-"""
-
-helps['vmware delete'] = """
+helps['vmware private-cloud delete'] = """
     type: command
     short-summary: Delete a private cloud.
 """
 
-helps['vmware show'] = """
+helps['vmware private-cloud deleteauthorization'] = """
+    type: command
+    short-summary: Delete an authorization to the ExpressRoute for a private cloud.
+"""
+
+helps['vmware private-cloud deleteidentitysource'] = """
+    type: command
+    short-summary: Delete a vCenter Single Sign On Identity Source for a private cloud.
+"""
+
+helps['vmware private-cloud list'] = """
+    type: command
+    short-summary: List the private clouds.
+"""
+
+helps['vmware private-cloud listadmincredentials'] = """
+    type: command
+    short-summary: List the admin credentials for the private cloud.
+"""
+
+helps['vmware private-cloud show'] = """
     type: command
     short-summary: Show details of a private cloud.
 """
 
-helps['vmware update'] = """
+helps['vmware private-cloud update'] = """
     type: command
     short-summary: Update a private cloud.
 """
--- azext_vmware/_params.py
+++ azext_vmware/_params.py
@@ -18,9 +18,38 @@ def load_arguments(self, _):
     with self.argument_context('vmware private-cloud') as c:
         c.argument('circuit_primary_subnet', help='A /30 subnet for the primary circuit in the Express Route to configure routing between your network and Microsoft\'s Enterprise edge (MSEEs) routers.')
         c.argument('circuit_secondary_subnet', help='A /30 subnet for the secondary circuit in the Express Route to configure routing between your network and Microsoft\'s Enterprise edge (MSEEs) routers.')
-        c.argument('cluster_size', help='Number of hosts for the new cluster. Minimum 4, Maximum 16.')
-        c.argument('network_block', help='A subnet at least of size /22.')
+        c.argument('cluster_size', help='Number of hosts for the default management cluster. Minimum of 3 and maximum of 16.')
+        c.argument('network_block', help='A subnet at least of size /22. Make sure the CIDR format is conformed to (A.B.C.D/X) where A,B,C,D are between 0 and 255, and X is between 0 and 22.')
 
     with self.argument_context('vmware cluster') as c:
         c.argument('parent_resource_name', options_list=['--parent-resource-name', '-p'], help='Name of the parent resource, the name of the private cloud.')
-        c.argument('size', help='Number of hosts for the new cluster.')
+        c.argument('size', help='Number of hosts for the cluster. Minimum of 3 and a maximum of 16.')
+
+    with self.argument_context('vmware private-cloud addauthorization') as c:
+        c.argument('authorization_name', help='Name of the authorization.')
+
+    with self.argument_context('vmware private-cloud addidentitysource') as c:
+        c.argument('alias', help='The domain\'s NetBIOS name.')
+        c.argument('base_group_dn', help='The base distinguished name for groups.')
+        c.argument('base_user_dn', help='The base distinguished name for users.')
+        c.argument('domain', help='The domain\'s dns name.')
+        c.argument('name', help='The name of the identity source.')
+        c.argument('password', help='The password of the Active Directory user with a minimum of read-only access to Base DN for users and groups.')
+        c.argument('primary_server', help='Primary server URL.')
+        c.argument('secondary_server', help='Secondary server URL.')
+        c.argument('ssl', help='Protect LDAP communication using SSL certificate (LDAPS). Specify "Enabled" or "Disabled".')
+        c.argument('username', help='The ID of an Active Directory user with a minimum of read-only access to Base DN for users and group.')
+
+    with self.argument_context('vmware private-cloud create') as c:
+        c.argument('internet', help='Connectivity to internet. Specify "Enabled" or "Disabled".')
+
+    with self.argument_context('vmware private-cloud deleteauthorization') as c:
+        c.argument('authorization_name', help='Name of the authorization.')
+
+    with self.argument_context('vmware private-cloud deleteidentitysource') as c:
+        c.argument('alias', help='The domain\'s NetBIOS name.')
+        c.argument('domain', help='The domain\'s dns name.')
+        c.argument('name', help='The name of the identity source.')
+
+    with self.argument_context('vmware private-cloud update') as c:
+        c.argument('internet', help='Connectivity to internet. Specify "Enabled" or "Disabled".')
